3f6e3226b09675159ca9965cdaf4b3266255f766
doremon11 <72811328+doremon11@users.noreply.github.com>
2022-07-17 01:27:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,69 @@
+import img1 from "./img/img1.png";
+import img2 from "./img/img2.png";
+import img3 from "./img/img3.png";
+import img4 from "./img/img4.png";
+import img5 from "./img/img5.png";
+import img6 from "./img/img6.png";
+import img7 from "./img/img7.png";
+import img8 from "./img/img8.png";
+const data={
+    productdata:[
+        {
+          id : 1,
+          img : img1,
+          title : "grape",
+          desc : '',
+          price : 56
+        },
+        {
+            id : 2,
+            img : img2,
+            title : "watermelon",
+            desc : '',
+            price : 70
+          },
+          {
+            id : 3,
+            img : img3,
+            title : "mango",
+            desc : '',
+            price : 40
+          },
+          {
+            id : 4,
+            img : img4,
+            title : "cherry",
+            desc : '',
+            price : 45
+          },
+          {
+              id : 5,
+              img : img5,
+              title : "pineapple",
+              desc : '',
+              price : 60
+            },
+            {
+              id : 6,
+              img : img6,
+              title : "apple",
+              desc : '',
+              price : 30
+            },
+            {
+              id : 7,
+              img : img7,
+              title : "strawberry",
+              desc : '',
+              price : 60
+            },
+            {
+              id : 8,
+              img : img8,
+              title : "Guava",
+              desc : '',
+              price : 30
+            }
+    ],
+};
+export default data;
